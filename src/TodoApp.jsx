26b1ffea46ecd7780d48d962c2d70b28859696c0
Jhonni10 <jhonieripianoscue@gmail.com>
2023-08-11 10:36:11 -0500

    rtk - query

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,27 +1,33 @@
+import { useState } from "react";
 import { useGetTodoQuery } from "./store/apis/todosApi";
 
 const TodoApp = () => {
 
-  const {data: todos = [] , isLoading , } = useGetTodoQuery(1);
+
+  const [state, setState] = useState(1);
+  const {isLoading , data : todo} = useGetTodoQuery(state);
+
+  const handleNext = ()=>{
+    setState(state + 1)
+  }
+
+  const handlePrev = ()=>{
+    if (state <= 1) return
+    setState(state - 1)
+  }
   
     return (
         <>
             <h1>Todo App</h1>
             <hr/>
             <h4>isLoading...{isLoading ?'true':'false'}</h4>
-            <pre>...</pre>
-            <ul>
-            {
-                todos.map(todo => (
-                    <li key={todo.id}>
-                       <strong>{todo.completed ? 'done':'pending'}</strong> {todo.title}
-                    </li>
-                ))
-            }
+            <pre>{JSON.stringify(todo)}</pre>
 
-            </ul>
+            
 
-            <button>Next todo</button>
+            <button onClick={handlePrev}>Prev todo</button>
+            <button onClick={handleNext}>Next todo</button>
+
         </>
     );
 }
